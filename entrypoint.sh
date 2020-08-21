98f8dccd245619f7e58bece8d8d3dec6f43a64b1
fnbellomo <fnbellomo@gmail.com>
2020-08-21 10:07:57 -0300

    Add and use a base common spider

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh -l
 
-echo "Hello $1"
+echo "Spider path $1"
+echo "Spider name $2"
 
-scrapy crawl quotes
+cp $1 ./baseSpider/baseSpider/spiders/
+cd baseSpider
+scrapy crawl $2
 
 time=$(date)
 echo "::set-output name=time::$time"
