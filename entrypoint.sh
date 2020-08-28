08d03c7cd5cab575dc5a4c348640adb802570df1
fnbellomo <fnbellomo@gmail.com>
2020-08-27 23:11:27 -0300

    fix bash script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo $SPIDER_PATH $AUTOCOMMIT
 
 scrapy runspider $SPIDER_PATH
 
-if [ -z "$AUTOCOMMIT" ] then
+if [[ -z "$AUTOCOMMIT" ]]; then
   git config --global user.name 'auto commit'
   git config --global user.email 'auto-commit@users.noreply.github.com'
   # git add fibertel
