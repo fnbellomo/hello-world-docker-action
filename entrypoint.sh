166839b196de804bf389756c9d97907a52d0b8fb
fnbellomo <fnbellomo@gmail.com>
2020-08-27 23:31:45 -0300

    the scrip now use bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,14 @@
-#!/bin/sh -l
-
-echo $SPIDER_PATH $AUTOCOMMIT
+#!/usr/bin/env bash
 
 scrapy runspider $SPIDER_PATH
 
-if [[ -z "$AUTOCOMMIT" ]]; then
+if [[ -n "$AUTOCOMMIT" ]]; then
+  echo "autocommit the result"
   git config --global user.name 'auto commit'
   git config --global user.email 'auto-commit@users.noreply.github.com'
   # git add fibertel
   git commit -am "Automated commit push"
   git push
+else
+  echo "no commit the result"
 fi
